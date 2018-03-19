a76f79cf1156b0c093dee9e262fb408f95c9a9ed
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-03-19 17:48:20 +0900

    異性詳細ページ作成の為の初期設定
    ・ルーティング
    ・コントローラー
    ・メインビュー（search_one）
    ・部分テンプレート（_search_one）
    ・scss

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pairs#index'
   get  "myprofile" => "pairs#myprofile"
+  get  "searchone" => "pairs#search_one"
 
   get 'like' => 'pairs#from_partner'
   get 'messages' => 'pairs#messages_index'
