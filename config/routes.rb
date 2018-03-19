90726ec82b56052d8e6512297a6bd2b01485b175
谷口晶紀 <owner@taniguchiaki-no-MacBook-Pro.local>
2018-03-19 16:23:20 +0900

    likes_edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pairs#index'
   get 'messages' => 'pairs#messages_index'
-  get 'like' => 'pairs#from_partner'
+  get 'like/from_partner' => 'pairs#from_partner'
+  get 'like/from_me'  => 'pairs#from_me'
+  get 'like/favorite' => 'pairs#favorite'
 
 end
