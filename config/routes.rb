7c71b447bdd398ab22eff949811ae3c0d0070295
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-03-19 15:02:27 +0900

    いいね機能とマッチング機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    member do
+     get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pairs#index'
+
+  resources:relationships, only: [:create, :destroy]
 end
