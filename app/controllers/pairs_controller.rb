0a716ebcf52212f4f58c3b4c076f2b365b71ff8e
原田悠佑 <adsoofmelk1327@gmail.com>
2018-03-21 19:52:12 +0900

    mastermerge to loginview

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,17 +1,27 @@
 class PairsController < ApplicationController
 
   def index
-    @users = User.page(params[:page]).per(1)
+    @usersPage = User.page(params[:page]).per(1)
+    @users = User.all
+    @relationship = Relationship.new
   end
   def messages_index
 
   end
 
+  def search_one
+  end
 
   def myprofile
   end
 
   def from_partner
 
+  end
+
+  def visitor_list
+  end
+
+  def visitor_setting
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    member do
+     get :following, :followers
+    end
+  end
+
   root 'pairs#index'
   get 'messages' => 'pairs#messages_index'
+  resources :relationships, only: [:create, :destroy]
   get 'like/from_partner' => 'pairs#from_partner'
   get 'like/from_me'  => 'pairs#from_me'
   get 'like/favorite' => 'pairs#favorite'
-  get  "myprofile" => "pairs#myprofile"
+  get "myprofile" => "pairs#myprofile"
+  get "searchone" => "pairs#search_one"
+  get "visitorlist" => "pairs#visitor_list"
+  get "visitorsetting" => "pairs#visitor_setting"
 end
