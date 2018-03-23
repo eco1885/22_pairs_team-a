1a1b0aadc0211b273723493a0465b615c36a4799
原田悠佑 <adsoofmelk1327@gmail.com>
2018-03-23 18:00:26 +0900

    create user show.html.haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
+    @usersPage = User.page(params[:page]).per(1)
+    @users = User.all
+    @relationship = Relationship.new
   end
 
 end
