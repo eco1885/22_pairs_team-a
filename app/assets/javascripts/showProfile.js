68bfd80ebba1c3c4ad8b20b8b143d16df10c4ee4
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-03-24 19:04:33 +0900

    Jsによるユーザーの情報が入ったプロフィールの表示

--- app/assets/javascripts/showProfile.js
+++ app/assets/javascripts/showProfile.js
@@ -1,11 +1,19 @@
 $(function() {
   $(".showPersonalViewModal").on("click", function(e){
     e.preventDefault();
-    $(".bg_overlay").fadeIn(800);
+    $(".bg_overlay").fadeIn(300);
+    var imageId = $(this).data("id")
+    $(".showProfile").each(function(){
+      if ($(this).data("id") == imageId){
+        $(this).fadeIn(800)
+      }
+    })
+
   });
-  $(".modal_close").on("click", function(e){
+  $(".showProfile").on("click", function(e){
     e.preventDefault();
     $(".bg_overlay").fadeOut(500);
+    $(".showProfile").fadeOut(100);
   });
 });
 
