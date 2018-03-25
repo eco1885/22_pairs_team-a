c5ed20c23a23957fd453ec624a198c0bbab13ded
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-03-25 21:20:07 +0900

    ページ遷移した際のjsが発火しない問題の修正

--- app/assets/javascripts/showProfile.js
+++ app/assets/javascripts/showProfile.js
@@ -1,26 +1,27 @@
-$(function() {
-  $(".showPersonalViewModal").on("click", function(e){
-    e.preventDefault();
-    $(".js_click_hide").hide();
-    $(".bg_overlay").fadeIn(300);
-    var imageId = $(this).data("id")
-    $(".showProfile").each(function(){
-      if ($(this).data("id") == imageId){
-        $(this).fadeIn(800)
+$(document).on('turbolinks:load',function(){
+  $(function() {
+    $(".showPersonalViewModal").on("click", function(e){
+      e.preventDefault();
+      $(".js_click_hide").hide();
+      $(".bg_overlay").fadeIn(300);
+      var imageId = $(this).data("id")
+      $(".showProfile").each(function(){
+        if ($(this).data("id") == imageId){
+          $(this).fadeIn(800)
+        }
+      })
+
+    });
+    $(".modal_close").on("click", function(e){
+      e.preventDefault();
+      $(".bg_overlay").fadeOut(500);
+      $(".showProfile").fadeOut(100);
+    });
+    $(".bg_overlay").on('click', function(e) {
+      if (!$(e.target).closest('.showProfile').length) {
+      $(".bg_overlay").fadeOut(500);
+      $(".showProfile").fadeOut(100);
       }
-    })
-
+    });
   });
-  $(".modal_close").on("click", function(e){
-    e.preventDefault();
-    $(".bg_overlay").fadeOut(500);
-    $(".showProfile").fadeOut(100);
-  });
-  $(".bg_overlay").on('click', function(e) {
-    if (!$(e.target).closest('.showProfile').length) {
-    $(".bg_overlay").fadeOut(500);
-    $(".showProfile").fadeOut(100);
-    }
-  });
-});
-
+})
