acce688cd134270db8cef13064b645ad845e8f18
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2018-03-26 11:37:20 +0900

    無駄な記述の削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-# ActiveRecord::Schema.define(version: 20180321073909) do
-
 ActiveRecord::Schema.define(version: 20180322071513) do
 
   create_table "foots", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
