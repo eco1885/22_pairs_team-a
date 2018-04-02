4d22d4849eaf36662c9225f79972da7bae911cb4
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-02 15:12:56 +0900

    マイページのコミュニティをカレントユーザーと関連付け

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     # @users = User.all
     # @relationship = Relationship.new
     @image = UserImage.new
+    @communities = current_user.communities.page(params[:page]).order("created_at DESC").per(12)
   end
 
 end
